073b34598e2ca660a02097ac6a70355fedf2a319
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-03-07 22:26:38 +0000

    Began solving DMOJ problem 'DWITE '12 R1 #4 - Trick or Tree'ing'
    Created `CountCandy` method that traverses a tree using a Stack for backtracking.

---
+++ TrickOrTreet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace TrickOrTreet
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TrickOrTreetSolver
+    {
+        public static int CountCandy(BinaryTreeNode<int?> tree)
+        {
+            int total = 0;
+            var stack = new Stack<BinaryTreeNode<int?>>();
+
+            while (tree != null)
+            {
+                if (tree.Left != null && tree.Right != null)
+                {
+                    stack.Push(tree.Left);
+                    tree = tree.Right;
+                }
+                else
+                {
+                    total += tree.Value ?? 0;
+                    tree = stack.Count == 0 ? null : stack.Pop();
+                }
+            }
+
+            return total;
+        }
+    }
+
+    public class BinaryTreeNode<T>
+    {
+        public BinaryTreeNode(T value = default(T))
+        {
+            Value = value;
+        }
+
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public T Value { get; set; }
+    }
+}
