18f36e559e5b1ec1fd71de4e32c63821bbf95ea5
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-03-10 00:02:50 +0000

    First successful submission for DM::OJ problem `ICPC ECNA 2005 B - Countdown`
    Resources: 0.078s, 23.95 MB
    
    https://dmoj.ca/submission/6277674

---
+++ FamilyCountdown/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FamilyTreeNode = FamilyCountdown.TreeNode<FamilyCountdown.FamilyMember>;
+
+namespace FamilyCountdown
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var numTestCases = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numTestCases; i++)
+            {
+                var testCaseArgs = Console.ReadLine().Split(' ');
+                var numFamilyMembers = int.Parse(testCaseArgs[0]);
+                var atDepth = int.Parse(testCaseArgs[1]);
+
+                var familyMemberReader = FamilyMemberReader(numFamilyMembers);
+                var results = FamilyCountdownSolver.Solve(familyMemberReader, atDepth);
+
+                Console.WriteLine($"Tree {i}:");
+                foreach (var result in results)
+                    Console.WriteLine(result);
+                Console.WriteLine();
+            }
+        }
+
+        private static IEnumerable<string[]> FamilyMemberReader(int numFamilyMembers)
+        {
+            for (int j = 0; j < numFamilyMembers; j++)
+            {
+                yield return Console.ReadLine().Split(' ');
+            }
+        }
+    }
+
+    public class TreeNode<T>
+    {
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public IList<TreeNode<T>> Children { get; set; }
+        public T Value { get; set; }
+    }
+
+    public class FamilyMember
+    {
+        public FamilyMember(string name, int score = 0)
+        {
+            Name =  name;
+            Score = score;
+        }
+
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public override string ToString() => $"{Name} {Score}";
+    }
+
+    public static class FamilyCountdownSolver
+    {
+        public static IEnumerable<FamilyMember> Solve(IEnumerable<string[]> familyMemberReader, int atDepth)
+        {
+            var treeNodes = BuildFamilyTree(familyMemberReader);
+            UpdateScores(treeNodes, atDepth);
+
+            var sortedNodes = from n in treeNodes
+                              orderby
+                                  n.Value.Score descending,
+                                  n.Value.Name ascending
+                              select n.Value;
+
+            return CollateResults(sortedNodes.ToList());
+        }
+
+        private static IEnumerable<FamilyMember> CollateResults(IList<FamilyMember> members)
+        {
+            int i = 0;
+            while (i < 3 && i < members.Count && members[i].Score > 0)
+            {
+                yield return members[i];
+                i += 1;
+
+                while (i < members.Count && members[i].Score == members[i - 1].Score)
+                {
+                    yield return members[i];
+                    i += 1;
+                }
+            }
+        }
+
+        public static IList<FamilyTreeNode> BuildFamilyTree(IEnumerable<string[]> familyMemberDataProvider)
+        {
+            var treeNodes = new List<FamilyTreeNode>();
+            var treeNodeMap = new Dictionary<string, FamilyTreeNode>();
+
+            foreach (var memberData in familyMemberDataProvider)
+            {
+                var memberName = memberData[0];
+                var numChildren = int.Parse(memberData[1]);
+                var memberNode = treeNodeMap.TryGetValue(memberName, out var existingMemberNode)
+                    ? existingMemberNode
+                    : CreateFamilyTreeNode(memberName);
+                memberNode.Children = new FamilyTreeNode[numChildren];
+
+                var i = 0;
+                foreach (var childName in memberData.Skip(2).Take(numChildren))
+                {
+                    var childNode = treeNodeMap.TryGetValue(childName, out var existingChildNode)
+                        ? existingChildNode
+                        : CreateFamilyTreeNode(childName);
+                    treeNodeMap[childNode.Value.Name] = childNode;
+                    memberNode.Children[i] = childNode;
+                    i += 1;
+                }
+
+                treeNodeMap[memberNode.Value.Name] = memberNode;
+                treeNodes.Add(memberNode);
+            }
+
+            return treeNodes;
+        }
+
+        private static FamilyTreeNode CreateFamilyTreeNode(string familyMemberName)
+        {
+            return new FamilyTreeNode(new FamilyMember(familyMemberName));
+        }
+
+        public static int CalcScore(FamilyTreeNode fromNode, int atDepth)
+        {
+            if (fromNode?.Children == null)
+                return 0;
+
+            if (atDepth == 1)
+                return fromNode.Children.Count;
+            
+            var total = 0;
+            foreach (var child in fromNode.Children)
+                total += CalcScore(child, atDepth - 1);
+
+            return total;
+        }
+
+        public static void UpdateScores(IEnumerable<FamilyTreeNode> nodes, int atDepth)
+        {
+            foreach (var node in nodes)
+            {
+                node.Value.Score = CalcScore(node, atDepth);
+            }
+        }
+    }
+}
---
+++ FamilyCountdownTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FamilyCountdownTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FamilyCountdownTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43d13ce5-67fe-4017-b06c-b8c145a34430")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
